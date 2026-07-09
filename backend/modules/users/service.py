02f8638c31270b634bed54337de7209e18ff678e
Hong-CHP <93changhong@gmail.com>
2026-07-09 03:00:47 +0000

    users service

--- backend/modules/users/service.py
+++ backend/modules/users/service.py
@@ -1,5 +1,6 @@
 from modules.users.repository import UsersRepository
-
+from modules.users.schemas import UserProfile
+from modules.users.exceptions import UserNotFoundException
 
 class UsersService:
     def __init__(
@@ -8,3 +9,13 @@ class UsersService:
     ):
         self.repository = repository
     
+    async def get_profile(
+            self,
+            current_user_id: int
+            ) -> UserProfile:
+        current_user = await self.repository.get_user_by_id(current_user_id)
+
+        if not current_user:
+            raise UserNotFoundException()
+        
+        return current_user
