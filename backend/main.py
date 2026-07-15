a47966cb85a94f5f7f325f1a85e5d552da608f34
Hong-CHP <93changhong@gmail.com>
2026-07-15 20:50:55 +0000

    add register tags exceptions handler and include tags router

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,16 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from modules.auth.handlers import register_auth_exception_handlers
 from modules.users.handlers import register_users_exception_handlers
+from modules.tags.handlers import register_tags_exception_handlers
 from core.database import db_lifespan
 from modules.auth.controller import auth_router
 from modules.users.controller import users_router
+from modules.tags.controller import tags_router
 
 app = FastAPI(title="Matcha API", version="1.0", lifespan=db_lifespan)
 register_auth_exception_handlers(app)
 register_users_exception_handlers(app)
+register_tags_exception_handlers(app)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://192.168.64.4:5173"],
@@ -19,6 +22,7 @@ app.add_middleware(
 
 app.include_router(auth_router)
 app.include_router(users_router)
+app.include_router(tags_router)
 
 @app.get("/health", tags=["System"])
 async def execute_health_check():
